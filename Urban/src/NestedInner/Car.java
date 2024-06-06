8ec4107282db8f07533d49de2bbf74e0b1c214f3
gukhman <gukhman@gmail.com>
2024-06-06 09:04:09 +0200

    NestedInnerWorks

--- Urban/src/NestedInner/Car.java
+++ Urban/src/NestedInner/Car.java
@@ -1,5 +1,6 @@
 package NestedInner;
 
+import java.util.Scanner;
 /*
 Создать класс Car(Машина). Написать поля carBrand (марка автомобиля), speed(скорость), weight(вес).
 Написать внутренние классы, характеризующие составные части автомобиля,
@@ -10,91 +11,153 @@ package NestedInner;
  */
 
 public class Car {
-    private String carBrand;
-    private int speed;
-    private int weight;
-    private int length;
+    String carBrand;
+    int speed;
+    int length;
 
-    public void move() {                //Для движения автомобиля запускаем двигатель
-        Engine engine = new Engine();
-        engine.Running();
+    public Car(String carBrand, int speed, int length) {
+        this.carBrand = carBrand;
+        this.speed = speed;
+        this.length = length;
     }
 
-    public class Engine {
-        private String numberOfCylinders;
-        private int weight;
+    private int maxRangeCalculation(int fuel) {
+        return fuel * 2;
+    }
 
-        public void Running() {
+    private boolean startMoving(int range, int maxRange) {
+        return range <= maxRange;
+    }
 
-            class EngineStrokes {       //Почему этому классу нельзя задать модификатор доступа?
-                                        //Почему не могу создать объекты этого класса?
-                                        //Работа двигателя описывается 4-мя тактами, ходом поршня вверх/вниз
-                                        //а также открытием и закрытием впускного и выпускного клапана
+    public void carStart() {                //Для движения автомобиля запускаем двигатель
+        CarBody carBody = new CarBody(1200);
+        Engine engine = new Engine(4, 400);
 
+        System.out.println("Едем на автомобиле " + carBrand + " с макимальной скоростью " + speed + " км/ч");
+        int c = engine.EngineNumberOfCylinders();
+        int w = engine.EngineWeight() + carBody.carBodyWeight();
+        System.out.println("Автомобиль длиной " + length + "мм с общим весом " + w + "кг, включая вес " + c + "-хцилиндрового двигателя и кузова ");
 
-                public void Intake() {      //Впуск
-                    System.out.println("Такт впуска:");
-                    EngineStrokes intakeStroke = new EngineStrokes();
-                    IntakeValve.open();
-                    ExhaustValve.close();
-                    intakeStroke.PistonMoveDown();
-                }
+        System.out.print("Сколько литров топлива в баке?: ");
+        Scanner in = new Scanner(System.in);
+        int fuel = in.nextInt();
+        if (fuel < 1) {
+            System.out.println("Добавьте топлива!");
+            System.exit(0);
+        }
 
-                public void Compression() {   //Сжатие
-                    System.out.println("Такт сжатия:");
-                    EngineStrokes compressionStroke = new EngineStrokes();
-                    IntakeValve.close();
-                    ExhaustValve.close();
-                    compressionStroke.PistonMoveDown();
-                }
+        int maxRange = maxRangeCalculation(fuel);
 
-                public void Combustion() {   //Рабочий ход
-                    System.out.println("Такт рабочего хода:");
-                    EngineStrokes combustionStroke = new EngineStrokes();
-                    IntakeValve.close();
-                    ExhaustValve.open();
-                    combustionStroke.PistonMoveDown();
-                }
+        System.out.print("Длина маршрута в километрах(запаса топлива хватит на " + maxRange + " км): ");
+        Scanner in1 = new Scanner(System.in);
+        int range = in1.nextInt();       //Километраж поездки
 
-                public void Exhausting() {   //Выпуск
-                    System.out.println("Такт выпуска:");
-                }
-
-
-                private void PistonMoveUp() {
-                    System.out.println("Поршень движется вверх");
-                }
-
-                private void PistonMoveDown() {
-                    System.out.println("Поршень движется вниз");
-                }
-
-                private class IntakeValve {
-                    static private void open() {
-                        System.out.println("Впускной клапан открыт");
-                    }
-
-                    static private void close() {
-                        System.out.println("Впускной клапан закрыт");
-                    }
-                }
-
-                private class ExhaustValve {
-                    static private void open() {
-                        System.out.println("Выпускной клапан открыт");
-                    }
-
-                    static private void close() {
-                        System.out.println("Выпускной клапан закрыт");
-                    }
-                }
-
+        if (startMoving(range, maxRange)) {
+            System.out.println("$Начинаем поездку. Ниже будут показаны такты двигателя:");
+            int i = 1;                       //одометр
+            while (i <= range) {
+                engine.Running(i);
+                i++;
             }
+            System.out.println("$Мы прибыли в пункт назначения!");
+        } else {
+            System.out.println("Не хватит топлива для поездки");
         }
     }
 
-    public class carBody {
-        private int length;
-        private int weight;
+    private class Engine {
+        int numberOfCylinders;
+        int weight;
+
+        public Engine(int numberOfCylinders, int weight) {
+            this.numberOfCylinders = numberOfCylinders;
+            this.weight = weight;
+        }
+
+        public int EngineWeight() {
+            return weight;
+        }
+
+        public int EngineNumberOfCylinders() {
+            return numberOfCylinders;
+        }
+
+        public void Running(int Range) {
+
+            class EngineStrokes {
+                //Работа двигателя описывается 4-мя тактами, ходом поршня вверх/вниз
+                //а также открытием и закрытием впускного и выпускного клапана
+
+                private void PistonMoveUp() {
+                    System.out.println("    Поршень движется вверх");
+                }
+
+                private void PistonMoveDown() {
+                    System.out.println("    Поршень движется вниз");
+                }
+
+                private void intakeOpen() {
+                    System.out.println("    Впускной клапан открыт");
+                }
+
+                private void intakeClose() {
+                    System.out.println("    Впускной клапан закрыт");
+                }
+
+                private void exhaustOpen() {
+                    System.out.println("    Выпускной клапан открыт");
+                }
+
+                private void exhaustClose() {
+                    System.out.println("    Выпускной клапан закрыт");
+                }
+
+                private void Intake() {      //Впуск
+                    System.out.println("--Такт впуска:");
+                    intakeOpen();
+                    exhaustClose();
+                    PistonMoveDown();
+                }
+
+                private void Compression() {   //Сжатие
+                    System.out.println("--Такт сжатия:");
+                    intakeClose();
+                    exhaustClose();
+                    PistonMoveUp();
+                }
+
+                private void Combustion() {   //Рабочий ход
+                    System.out.println("--Такт рабочего хода:");
+                    intakeClose();
+                    exhaustClose();
+                    PistonMoveDown();
+                }
+
+                private void Exhaust() {   //Выпуск
+                    System.out.println("--Такт выпуска:");
+                    intakeClose();
+                    exhaustOpen();
+                    PistonMoveUp();
+                }
+            }
+            EngineStrokes intakeStroke = new EngineStrokes();
+            intakeStroke.Intake();
+            intakeStroke.Compression();
+            intakeStroke.Combustion();
+            intakeStroke.Exhaust();
+            System.out.println("!Автомобиль проехал " + Range + " км");
+        }
+    }
+
+    private class CarBody {
+        int weight;
+
+        public CarBody(int weight) {
+            this.weight = weight;
+        }
+
+        public int carBodyWeight() {
+            return weight;
+        }
     }
 }
--- Urban/src/NestedInner/Start.java
+++ Urban/src/NestedInner/Start.java
@@ -2,7 +2,7 @@ package NestedInner;
 
 public class Start {
     public static void main(String[] args) {
-        Car car = new Car();
-        car.move();
+        Car car = new Car("Lada",160,4300);
+        car.carStart();
     }
 }
