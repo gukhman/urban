f7b0ad40447c02320b22108b3f8f587193b68f7a
gukhman <gukhman@gmail.com>
2024-06-08 11:07:46 +0200

    Generics

--- Urban/src/Generics/G2/Main.java
+++ Urban/src/Generics/G2/Main.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String[] arr = {"1", "2", "3"};
+        //Integer[] arr = {1, 2, 3};      //Работает
+        String[] arr = {"1", "2", "3"};       //Тоже работает
         System.out.println("Исходный массив: " + Arrays.toString(arr));
-        String out = "";
-        Generic generic = new Generic<String>(arr);
+        Generic generic = new Generic<>(arr);
         Object[] arr2 = generic.change(0, 2);
         System.out.println("Массив со сменой элементов: " + Arrays.toString(arr2));
     }
