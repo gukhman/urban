a0d99f9178a31b3964f99f4253ba95e78cc9c3d6
gukhman <gukhman@gmail.com>
2024-06-10 07:25:27 +0200

    Collections

---
+++ Urban/src/Collections/main.java
@@ -0,0 +1,20 @@
+package Collections;
+
+import java.util.ArrayList;
+
+public class main {
+    public static void main(String[] args) {
+        ArrayList<String> colors;
+        colors = new ArrayList<>();
+        colors.add("red");
+        colors.add("green");
+        colors.add("blue");
+        colors
+    }
+
+    public boolean test(String whatToSearch){
+
+        boolean res = false;
+        return res;
+    }
+}
