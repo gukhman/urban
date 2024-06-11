06a61d9e5a97b5042080ba002fbde8ed2435acaf
gukhman <gukhman@gmail.com>
2024-06-11 07:55:58 +0200

    HashSet2

--- Urban/src/Sets/intHashSet/Main.java
+++ Urban/src/Sets/intHashSet/Main.java
@@ -1,14 +1,14 @@
 package Sets.intHashSet;
 
+import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedHashSet;
 
 /*Создайте множество целых чисел. Необходимо вывести в консоль все его элементы, которые больше 15 и являются четными.
 Остальные элементы набора тоже необходимо вывести в консоль, но каждый из них разделив на 2*/
 public class Main {
     public static void main(String[] args) {
-        LinkedHashSet<intHashSet> intHashSet = new LinkedHashSet<>();
-        LinkedHashSet<intHashSet> resHashSet = new LinkedHashSet<>();
+        HashSet<intHashSet> intHashSet = new HashSet<>();
+        HashSet<intHashSet> resHashSet = new HashSet<>();
         for (int i = 10; i < 21; i++) {
             intHashSet.add(new intHashSet(i));
         }
--- Urban/src/Sets/intHashSet/intHashSet.java
+++ Urban/src/Sets/intHashSet/intHashSet.java
@@ -1,30 +1,14 @@
 package Sets.intHashSet;
 
-import java.util.Iterator;
-
-public class intHashSet implements Iterator{
-    private int item;
+public class intHashSet {
+    private final int item;
 
     public intHashSet(int item) {
         this.item = item;
     }
 
     public boolean isOdd() {
-        boolean result = false;
-        if (item % 2 == 0) {
-            result = true;
-        }
-        return result;
-    }
-
-    @Override
-    public boolean hasNext() {
-        return false;
-    }
-
-    @Override
-    public Object next() {
-        return null;
+        return item % 2 == 0;
     }
 }
 
