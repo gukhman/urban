3c43c34a7e538d73d3895c85e2ecb7e6b15e7ab7
gukhman <gukhman@gmail.com>
2024-06-13 17:23:43 +0200

    Stream1_1_update

--- Urban/src/Stream1/Task1/Main.java
+++ Urban/src/Stream1/Task1/Main.java
@@ -2,6 +2,7 @@ package Stream1.Task1;
 /*Создайте список типа String, например, названия пищевых продуктов.
 Напишите программу для подсчета количества строк в списке,
 начинающихся с определенной буквы, с использованием потоков.*/
+
 import java.util.stream.Stream;
 
 public class Main {
@@ -9,7 +10,11 @@ public class Main {
         //Создаем поток
         Stream<String> food = Stream.of("Банан", "Хлеб", "Мясо", "Огурец",
                 "Курица", "Макадамия", "Творог", "Апельсин");
-        //Выведем продукты, начинающиеся с буквы "М", используя regex
-        food.filter(x->x.matches("М.*")).forEach(x-> System.out.println(x));
+        char let = 'М';
+        String reg = let + ".*";
+        //Выведем Количество продуктов, начинающихся с буквы указанной
+        // в переменной let, используя regex и count()
+        System.out.println("Количество слов, начинающихся на букву "
+                + let + ": " + food.filter(x -> x.matches(reg)).count());
     }
 }
